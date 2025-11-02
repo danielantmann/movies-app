a797a18511412efb18ee025867444b1305dadc35
Daniel Eric Antmann <tula3x3@gmail.com>
2025-11-02 01:03:24 +0100

    feat(movie): create action, interface and mapper to fecth data from api and map it to custom response

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,8 @@
 import { Text, View } from 'react-native';
 import React, { Component } from 'react';
 import '../global.css';
-import { nowPlayingAction } from '@/core/actions/movies/now-playing.actions';
 
 const RootLayout = () => {
-  nowPlayingAction();
   return (
     <View>
       <Text>RootLayout</Text>
--- core/actions/movies/now-playing.actions.ts
+++ core/actions/movies/now-playing.actions.ts
@@ -1,11 +1,12 @@
 import { movieApi } from '@/core/api/movie-api';
 import { MovieDBMovieResponse } from '@/infrastructure/interfaces/moviedb-response';
+import { MovieMapper } from '@/infrastructure/mappers/movie.mapper';
 
 export const nowPlayingAction = async () => {
   try {
     const { data } = await movieApi.get<MovieDBMovieResponse>('/now_playing');
-    console.log(JSON.stringify(data, null, 2));
-    return [];
+    const movies = data.results.map(MovieMapper.fromMovieDBToMovie);
+    return movies;
   } catch (error) {
     console.log(error);
     throw 'CAnnot load now playing movies';
